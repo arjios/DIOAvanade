9ec6110bf2e1e8fd2fd65fbb70257183d9da9222
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-12-17 13:11:14 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+    namespace primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Feliz";
+            var b = " Natal";
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
